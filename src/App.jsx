3ef000f1c4f787c9097643cb8e4377e2bf26a365
TartCodes <tartcodes@gmail.com>
2023-01-26 14:08:22 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./Components/Layout/Header";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
+			<Header />
 		</div>
 	);
 }
--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import classes from "./Header.module.css";
+import meals from "/src/assets/images/meals.jpg";
 
-const Header = () => {
-	return <div>Header</div>;
+const Header = (props) => {
+	return (
+		<>
+			<header className={classes.header}>
+				<h1>ManyMeals</h1>
+				<button>Cart</button>
+			</header>
+			<div className={classes["main-image"]}>
+				<img
+					className={classes.img}
+					src={meals}
+				></img>
+			</div>
+		</>
+	);
 };
 
 export default Header;
