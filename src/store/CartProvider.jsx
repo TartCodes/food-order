759c3c58782cd579c030d1431970fc664fdf3871
TartCodes <tartcodes@gmail.com>
2023-02-02 14:51:23 -0500

    reducer

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,12 +1,32 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+	items: [],
+	totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+	if (action.type === "ADD") {
+		//use .concat() instead of .push() bc push edits the old array while concat creates a new one, you want your state to be immutable
+		const updatedItems = state.items.concat();
+	}
+
+	return defaultCartState;
+};
 
 const CartProvider = (props) => {
-	const addItem = (item) => {};
-	const removeItem = (id) => {};
+	const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+	const addItem = (item) => {
+		dispatchCart({ type: "ADD", item: item });
+	};
+	const removeItem = (id) => {
+		dispatchCart({ type: "REMOVE", id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItem,
 		removeItem: removeItem
 	};
